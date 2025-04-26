e90cbda8869b82ed21b65a7e81f61774b4425a7a
Somesh Raj <someshraj78569@gmail.com>
2025-04-26 13:07:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://skoolwhiz-backend-author-somesh-raj.vercel.app'
+    apiUrl: 'https://skoolwhiz-backend-author-somesh-raj.vercel.app/'
 };
